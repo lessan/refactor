1dd81f544c3e8d03846731c96580cf56a6810ebe
Lessan Vaezi <lessan@gmail.com>
2019-02-17 11:18:08 +1030

    When a folder is renamed, also replace the text within files inside that folder

--- lib/refactor.rb
+++ lib/refactor.rb
@@ -19,50 +19,56 @@ module Refactor
     # TODO handle TitleCase
 
     # all files in current directory
-    Dir.glob('**/*') do |old_path|
-      # ignore certain directories
-      unless old_path =~ %r{\A(coverage|pkg|tmp|vendor)(\z|/)}
-        # only check the basename so that the directory doesn't get renamed twice
-        old_basename = File.basename(old_path)
-        new_basename = old_basename.dup
-        new_basename.gsub!(dashed_regex, to_dashed)
-        new_basename.gsub!(underscored_regex, to_underscored)
+    paths = Dir.glob('**/*')
 
-        if new_basename == old_basename
-          # no change
-          new_path = old_path
-        else
-          # rename file
-          new_path = File.join(File.dirname(old_path), new_basename)
-          puts "#{old_path} –> #{new_path}"
-          FileUtils.mv(old_path, new_path)
+    # ignore certain directories
+    paths.reject! { |p| p =~ %r{\A(coverage|pkg|tmp|vendor)(\z|/)} }
+
+    # do folders last, so the contents of files within each folder are also addressed
+    paths = paths.partition { |p| File.file?(p) }.reduce(:+)
+
+    paths.each do |old_path|
+
+      # only check the basename so that the directory doesn't get renamed twice
+      old_basename = File.basename(old_path)
+      new_basename = old_basename.dup
+      new_basename.gsub!(dashed_regex, to_dashed)
+      new_basename.gsub!(underscored_regex, to_underscored)
+
+      if new_basename == old_basename
+        # no change
+        new_path = old_path
+      else
+        # rename file
+        new_path = File.join(File.dirname(old_path), new_basename)
+        puts "#{old_path} –> #{new_path}"
+        FileUtils.mv(old_path, new_path)
+      end
+
+      if File.file?(new_path)
+        # replace within file
+        old_text = File.read(new_path)
+        # http://robots.thoughtbot.com/fight-back-utf-8-invalid-byte-sequences
+        old_text.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+
+        new_text = old_text.dup
+        new_text.gsub!(camelized_regex, to_camelized)
+        new_text.gsub!(dashed_regex, to_dashed)
+        new_text.gsub!(underscored_regex, to_underscored)
+
+        unless new_text == old_text
+          # rewrite existing file
+          File.write(new_path, new_text)
         end
 
-        if File.file?(new_path)
-          # replace within file
-          old_text = File.read(new_path)
-          # http://robots.thoughtbot.com/fight-back-utf-8-invalid-byte-sequences
-          old_text.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
-
-          new_text = old_text.dup
-          new_text.gsub!(camelized_regex, to_camelized)
-          new_text.gsub!(dashed_regex, to_dashed)
-          new_text.gsub!(underscored_regex, to_underscored)
-
-          unless new_text == old_text
-            # rewrite existing file
-            File.write(new_path, new_text)
+        # show possible matches in body
+        line_num = 0
+        new_text.each_line do |old_line|
+          new_line = old_line.gsub(/#{Regexp.quote(from_humanized)}/i, "\e[33m\\0\e[0m")
+          unless new_line == old_line
+            puts "\e[36m#{new_path}:#{line_num}\e[0m #{new_line}"
           end
-
-          # show possible matches in body
-          line_num = 0
-          new_text.each_line do |old_line|
-            new_line = old_line.gsub(/#{Regexp.quote(from_humanized)}/i, "\e[33m\\0\e[0m")
-            unless new_line == old_line
-              puts "\e[36m#{new_path}:#{line_num}\e[0m #{new_line}"
-            end
-            line_num += 1
-          end
+          line_num += 1
         end
       end
     end
